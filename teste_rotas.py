2753f764fa29301ca7b55dace5e5b8cff20a7b0a
davichocoman <davichocoman18@gmail.com>
2025-03-27 15:17:55 +0000

    Alterei o layout da página base, criei uma página inicial com mais opções

--- teste_rotas.py
+++ teste_rotas.py
@@ -1,3 +1,35 @@
+# from flask import Flask, render_template, request, redirect, url_for
+
+
+# app = Flask(__name__,
+#             template_folder='nubes/app/templates',  # Pasta para templates
+#             static_folder='nubes/app/static',
+#             )       # Pasta para arquivos estáticos (CSS, JS, imagens)
+
+# @app.route('/')
+# def index():
+#     return render_template("layout.html")
+
+# # Rota para o login
+# @app.route('/login')
+# def login():
+#     if request.method == 'POST':
+#         # Processar os dados do formulário
+#         usuario = request.form.get('usuario')
+#         senha = request.form.get('senha')
+        
+#         # Aqui você pode adicionar a lógica de autenticação
+#         print(f"Usuário: {usuario}, Senha: {senha}")
+        
+#         # Redirecionar para uma página de sucesso (ou outra rota)
+#         return redirect(url_for('login'))  # Por enquanto, redireciona de volta para o login
+
+#     # Se for uma requisição GET, renderize a página de login
+#     return render_template('login.html')
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 from flask import Flask, render_template, request, redirect, url_for
 
 
@@ -6,26 +38,14 @@ app = Flask(__name__,
             static_folder='nubes/app/static',
             )       # Pasta para arquivos estáticos (CSS, JS, imagens)
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    pass
+
 @app.route('/')
 def index():
-    return render_template("layout.html")
+    return render_template("login.html")
 
-# Rota para o login
-@app.route('/login')
-def login():
-    if request.method == 'POST':
-        # Processar os dados do formulário
-        usuario = request.form.get('usuario')
-        senha = request.form.get('senha')
-        
-        # Aqui você pode adicionar a lógica de autenticação
-        print(f"Usuário: {usuario}, Senha: {senha}")
-        
-        # Redirecionar para uma página de sucesso (ou outra rota)
-        return redirect(url_for('login'))  # Por enquanto, redireciona de volta para o login
-
-    # Se for uma requisição GET, renderize a página de login
-    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
